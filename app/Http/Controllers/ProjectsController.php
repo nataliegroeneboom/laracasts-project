596ef5ead9e24530e73f0a4f07512329f67f8a5a
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-06-10 07:21:46 +0100

    tests on viewing and creating projects with authentication

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index(){
+        $projects = auth()->user()->projects;
+        return view('projects.index', compact('projects'));
+       
+    }
+
+    public function store(){
+        //validate
+       $attributes = request()->validate([
+           'title' => 'required', 
+           'description' => 'required'      
+           ]);
+
+        //  $attributes['owner_id'] = auth()->id();
+         auth()->user()->projects()->create($attributes);
+
+
+        // Project::create($attributes);
+        return redirect('/projects');
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show(Project $project){
+        if(auth()->user()->isNot($project->owner)){
+            abort(403);
+        }
+        return view('projects.show', compact('project'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
+Route::group(['middleware' => 'auth'], function(){
+    Route::post('/projects', 'ProjectsController@store');
+
+    Route::get('/projects', 'ProjectsController@index');
+    
+    Route::get('/projects/{project}', 'ProjectsController@show');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
